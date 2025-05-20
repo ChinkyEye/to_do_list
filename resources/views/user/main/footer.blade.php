470a618092244b2e30b387a7f0c9fb37d46014cf
Bipin Rai <bipinkirati61@gmail.com>
2025-05-20 13:33:35 +0100

    new code for software

--- resources/views/user/main/footer.blade.php
+++ resources/views/user/main/footer.blade.php
@@ -2,6 +2,6 @@
 	<strong>Copyright © 2020 @if(Date('Y') != 2020) - {{ Date('Y') }}@endif </strong>
 	All rights reserved.
 	<div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 1.0.0
+      <b>Version</b> 3.0.0
     </div>
 </footer>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>To-Do List</title>
+        <title>Ticket Booking</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -88,7 +88,7 @@
             </div> -->
             <div class="content">
                 <div class="title m-b-md" style="color:green;">
-                    Task Manager
+                    Ticket Booking
                 </div>
 
                 <!-- <div class="links">
